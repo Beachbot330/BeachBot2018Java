2627ba77fab166ee00c4497b23a34473a1df278b
trailblazer84562 <git@allenpeters.com>
2018-01-21 16:56:00 -0800

    Start working on lift subsystem

---
+++ BeachBot2018Java/src/org/usfirst/frc330/constants/LiftConst.java
@@ -0,0 +1,30 @@
+// Robot ARM Constants
+
+package org.usfirst.frc330.constants;
+
+import org.usfirst.frc330.Robot;
+import org.usfirst.frc330.wpilibj.PIDGains;
+
+public final class LiftConst {
+	
+	private LiftConst(){}
+
+	// Tolerance
+	public static final double tolerance        		= 5.0;
+
+	// PID Constants
+	public static final double proportional      		= 1.0;   // WAG
+	public static final double integral         		= 0.000; // WAG
+	public static final double derivative        		= 1.0;   // WAG
+	
+	// Poitions
+	public static final double limitLower   		=  2.0;  // WAG
+	public static final double limitUpper   		= 113.0; //WAG
+	
+	// Encoder Stuff - from 2016... remove?
+	public static final int    maxAngleDegrees   		= 72;    //JM 2-10
+	public static final int    maxEncoderCounts  		= -4096; //AP 2-20
+	public static final int    minQuadrant       		= 0;     //JM 2-10
+	public static final int    maxQuadrant       		= 4;     //JM 2-10
+
+}	
