f1fd83cd687e40cfae1befd92aedf059d56b8eda
Ollog10 <eli.olmos9@gmail.com>
2018-01-28 16:38:26 -0800

    imported files to intakeportal

--- src/org/usfirst/frc330/commands/commandgroups/IntakePortal.java
+++ src/org/usfirst/frc330/commands/commandgroups/IntakePortal.java
@@ -12,7 +12,10 @@
 package org.usfirst.frc330.commands.commandgroups;
 
 import edu.wpi.first.wpilibj.command.BBCommandGroup;
+import org.usfirst.frc330.commands.*;
+import org.usfirst.frc330.constants.*;
 import org.usfirst.frc330.subsystems.*;
+import org.usfirst.frc330.Robot;
 
 /**
  *
