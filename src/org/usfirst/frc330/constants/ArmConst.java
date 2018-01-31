c724c900fa899bb8c054ae45682ac856ff81c520
trailblazer84562 <git@allenpeters.com>
2018-01-30 17:34:01 -0800

    Ordered angles. Added WAG to untested values

--- src/org/usfirst/frc330/constants/ArmConst.java
+++ src/org/usfirst/frc330/constants/ArmConst.java
@@ -8,7 +8,6 @@
 
 package org.usfirst.frc330.constants;
 
-import org.usfirst.frc330.Robot;
 import org.usfirst.frc330.wpilibj.PIDGains;
 
 public final class ArmConst {
@@ -20,40 +19,38 @@ public final class ArmConst {
 
 	// Tolerance
 	public static final double tolerance        		= 5.0;
-	public static final double deadZone			 		= 0.05;  //AP 2-20
-	public static final double gamepadDeadZone   		= 0.10;  //AP 2-27
-	public static final double pickupTolerance	 		= 5.0; 	 //AP 2-6 (WAG)
-	public static final double defaultTimeout	 		= 5.0;	 //JR 2-28
-	public static final int    inertiaCounter	 		= 10;	 //JR 2-28 (loops after manual control before starting position control)
+	public static final double deadZone			 		= 0.05;  //AP WAG 1-30-18
+	public static final double gamepadDeadZone   		= 0.10;  //AP WAG 1-30-18
+	public static final double pickupTolerance	 		= 5.0; 	 //AP WAG 1-30-18
+	public static final double defaultTimeout	 		= 5.0;	 //AP WAG 1-30-18
+	public static final int    inertiaCounter	 		= 10;	 //AP WAG 1-30-18
 	
 	// PID Constants
-	public static final double proportional      		= 1.0;   // JR 2/23
-	public static final double integral         		= 0.000; // JR 2/23
-	public static final double derivative        		= 1.0;   // JR 2/23
-	public static final double feedForward	     		= 0.0;   // JR 2/23
-	public static final double VoltageRampRate   		= 48.0;  // AP 3-15
-	public static final double MaxOutputVoltage  		= 12.0;   // AP 3-15
+	public static final double proportional      		= 1.0;   // AP WAG 1-30-18
+	public static final double integral         		= 0.000; // AP WAG 1-30-18
+	public static final double derivative        		= 1.0;   // AP WAG 1-30-18
+	public static final double feedForward	     		= 0.0;   // AP WAG 1-30-18
+	public static final double VoltageRampRate   		= 48.0;  // AP WAG 1-30-18
+	public static final double MaxOutputVoltage  		= 12.0;  // AP WAG 1-30-18
 	public static final PIDGains fullPID = new PIDGains(proportional, integral, derivative, 
 														feedForward, MaxOutputVoltage, MaxOutputVoltage, "default");
 	
 	// Angles
-	public static final double intakePosition  			= -60;   //WAG JB 1/27
-	public static final double Defense 					= -60;   //WAG JB 1/27
-	public static final double leveledArm               =  0.0;  //MF 1/27 WAG
-	public static final double maxArm                   =  160.0; //EJO 1/28 WAG
 	public static final double minArm                   = -60.0; //MF 1/27 WAG
-	public static final double medArm                   =  0.0; //MF 1/27 WAG
-	public static final double switchArm                = 0.0; // MF 1/27 WAG needs value
-	public static final double climbPosition			= 90.0; // wag -EJO 1/27
+	public static final double intakePosition  			= -60.0; //WAG JB 1/27
+	public static final double Defense 					= -60.0; //WAG JB 1/27
 	public static final double portalPosition			= -60.0; //EJO 1/28
+	public static final double leveledArm               =   0.0; //MF 1/27 WAG
+	public static final double medArm                   =   0.0; //MF 1/27 WAG
+	public static final double switchArm                =   0.0; // MF 1/27 WAG needs value
+	public static final double climbPosition			=  90.0; // wag -EJO 1/27
+	public static final double maxArm                   = 160.0; //EJO 1/28 WAG
+	
 	
 	// Encoder Stuff
-	public static final int    maxAngleDegrees   		= 72;    //JM 2-10
-	public static final int    maxEncoderCounts  		= -4096; //AP 2-20
-	public static final int    minQuadrant       		= 0;     //JM 2-10
-	public static final int    maxQuadrant       		= 4;     //JM 2-10
+	public static final int    maxAngleDegrees   		= 72;    //AP WAG 1-30-18
+	public static final int    maxEncoderCounts  		= -4096; //AP WAG 1-30-18
+	public static final int    minQuadrant       		= 0;     //AP WAG 1-30-18
+	public static final int    maxQuadrant       		= 4;     //AP WAG 1-30-18
 
-	//Current
-	//public static final double currentLowerLimit = -50;
-	//public static final double currentUpperLimit = 50;
 }	
