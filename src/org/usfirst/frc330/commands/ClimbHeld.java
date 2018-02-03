5b6634508561aee29d61625502e7db09dff7d793
Ollog10 <eli.olmos9@gmail.com>
2018-02-03 14:47:33 -0800

    set up climbheld cmd

--- src/org/usfirst/frc330/commands/ClimbHeld.java
+++ src/org/usfirst/frc330/commands/ClimbHeld.java
@@ -38,6 +38,7 @@ public class ClimbHeld extends BBCommand {
     // Called just before this Command runs the first time
     @Override
     protected void initialize() {
+    	Robot.climber.climbHeld();
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -48,7 +49,7 @@ public class ClimbHeld extends BBCommand {
     // Make this return true when this Command no longer needs to run execute()
     @Override
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
