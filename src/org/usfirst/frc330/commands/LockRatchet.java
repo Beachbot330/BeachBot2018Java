a589f8158e295cf23e99ac58a279d40dda5f587f
Ollog10 <eli.olmos9@gmail.com>
2018-02-03 14:39:27 -0800

    added Lock/Unlock ratchet code to commands

--- src/org/usfirst/frc330/commands/LockRatchet.java
+++ src/org/usfirst/frc330/commands/LockRatchet.java
@@ -38,6 +38,7 @@ public class LockRatchet extends BBCommand {
     // Called just before this Command runs the first time
     @Override
     protected void initialize() {
+    	Robot.climber.lockRatchet();
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -48,7 +49,7 @@ public class LockRatchet extends BBCommand {
     // Make this return true when this Command no longer needs to run execute()
     @Override
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
