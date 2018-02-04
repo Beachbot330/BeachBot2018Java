b841b146b776fdd6cd4c19984d86a1237e71db19
Ollog10 <eli.olmos9@gmail.com>
2018-02-04 15:14:50 -0800

    added sensorMax/MinLength to GrabberConst

--- src/org/usfirst/frc330/constants/GrabberConst.java
+++ src/org/usfirst/frc330/constants/GrabberConst.java
@@ -12,7 +12,9 @@ public final class GrabberConst {
 	private GrabberConst(){}
 	
 	//Sensor
-	public static final double centerSensorPosition						= 9.5; //(inches) EJO 2.4.18
-	public static final double otherSensorDistanceFromCenter			= 4.0; //(inches) EJO 2.4.18
+	public static final double centerSensorPosition						= 9.5; 		//(inches) EJO 2.4.18
+	public static final double otherSensorDistanceFromCenter			= 4.0; 		//(inches) EJO 2.4.18
+	public static final double sensorMinLength							= 4.0; 		//(cm) EJO 2.4.18
+	public static final double sensorMaxLength							= 30.0;		//(cm) EJO 2.4.18
 	
 }	
