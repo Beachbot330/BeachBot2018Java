d04ce0bafebc5332fb0e4d5e13938b0450a1136d
Ollog10 <eli.olmos9@gmail.com>
2018-02-04 15:04:43 -0800

    added a GrabberConst file

---
+++ src/org/usfirst/frc330/constants/GrabberConst.java
@@ -0,0 +1,16 @@
+// Robot Grabber Constants
+
+
+
+package org.usfirst.frc330.constants;
+
+import org.usfirst.frc330.Robot;
+import org.usfirst.frc330.wpilibj.PIDGains;
+
+public final class GrabberConst {
+	
+	private GrabberConst(){}
+	
+	
+	
+}	
