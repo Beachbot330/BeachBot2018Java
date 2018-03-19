b80e481d2d19aad0634f2a4c89fe1d32d4c20e1f
sciencewhiz <git@rossesmail.com>
2018-03-18 21:51:23 -0700

    Convert lift to motion magic. PID set to 0 for tuning.

--- src/org/usfirst/frc330/constants/LiftConst.java
+++ src/org/usfirst/frc330/constants/LiftConst.java
@@ -11,9 +11,14 @@ public final class LiftConst {
 	public static final double defaultTimeout			= 3.0;  //AP WAG 3/4/18
 
 	// PID Constants
-	public static final double proportional      		= 0.2;   // AP 2/17/18
+	public static final double proportional      		= 0.0;   // JR 3/18/18 tuning for motion magic
 	public static final double integral         		= 0.000; // AP 3/3/18
 	public static final double derivative        		= 0.0;   // AP 3/3/18
+	public static final double feedforward              = 0.64;   // JR 3/18/18 max velocity of 1600 F = 1023/1600
+	
+	// Motion Magic Constants
+	public static final int velocityLimit              = 1500;   //JR 3/18/18
+	public static final int accelLimit                 = 1500;   //JR 3/18/18
 	
 	// Positions
 	public static final double lowerLimit				=  0.0; //AP 1-30-2018 WAG
@@ -42,7 +47,8 @@ public final class LiftConst {
 	public static final double MaxOutputPercent			= 1.0;	// AP 3/12/18
 	public static final int    CAN_Timeout				= 10; //AP 2/18/18
 	public static final int    CAN_Timeout_No_Wait      = 0;  // JR 2/19/18
-	public static final int    CAN_Status_Frame_13_Period = 20; //JR 2/19/19
+	public static final int    CAN_Status_Frame_13_Period = 20; //JR 2/19/18
+	public static final int    CAN_Status_Frame_10_Period = 20; //JR 3/18/18
 	
 	// Other
 	public static final double calibrationSpeed			= -0.15; //JR 3/11/18
